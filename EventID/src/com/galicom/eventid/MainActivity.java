15a0aa4937bbf9061256730ab3e4f87b3bb9f2bb
Mickael PERIL <2010690@campusid.eu>
2013-01-13 17:13:40 +0100

    Page de login
    ajout d'une foction qui permet de verifier si le champs email et
    password sont rempli ppour pouvoir continuer,  dans le cas échant un
    message d'erreur apparait.
    pour le momentles identifiant sont: email et password

--- EventID/src/com/galicom/eventid/MainActivity.java
+++ EventID/src/com/galicom/eventid/MainActivity.java
@@ -2,16 +2,27 @@ package com.galicom.eventid;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends Activity {
+	final Context context = this;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+
 	}
 
 	@Override
@@ -21,6 +32,8 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	// CREATION DES MENUE BOUTONS
+	// fonction qui va sur la page Signup lorsqu'on clique sur le bouton Signup
 	public void signupOnClick(View view)
 	{
 		Intent intent;
@@ -29,10 +42,43 @@ public class MainActivity extends Activity {
 	}
 	public void loginOnClick(View view)
 	{
-		Intent intent;
-		intent = new Intent(this, LoginActivity.class);
-		startActivity(intent);
+		final EditText vEditText1 = (EditText)findViewById(R.id.editText2); // declaration du champs text pour le mdp
+		final EditText vEditText2 = (EditText)findViewById(R.id.editText3); // declaration du champs text pour l'email
+
+		String vTexteEmail = vEditText2.getText().toString(); // récuperation du champs email
+		String vTextePassword = vEditText1.getText().toString();
+		
+		// verification des champs
+		if (vTexteEmail.equals("email")&& vTextePassword.equals("password") )
+		{
+			Intent intent;
+			intent = new Intent(this, LoginActivity.class);
+			startActivity(intent);
+		}
+
+		else{
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+			alertDialogBuilder.setTitle("incorrect authentication");
+			alertDialogBuilder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog,int id) {
+					dialog.cancel();
+				}
+			});
+			// creer la boite d'alert
+			AlertDialog alertDialog = alertDialogBuilder.create();
+
+			// affiche l'alerte
+			alertDialog.show();
+
+		}
+
 	}
 
-
 }
+
+
+
+
+
+
+
